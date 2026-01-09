9bf7b1a3a12d6c07de45b022f5a56b7f237c1abb
BNOBODY <brunofmouzinho12@gmail.com>
2026-01-09 22:21:55 +0100

    Adicionei comentários nas def's

--- Cliente.py
+++ Cliente.py
@@ -1,10 +1,14 @@
 import socket
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
-from datetime import date, time, datetime
+from datetime import date, datetime
 
 current_date = date.today()
 current_datetime = datetime.now()
+
+# ip = "172.20.10.10"
+ip = "127.0.0.1"
+port = 8080
 
 class ClientGUI:
     def __init__(self, root):
@@ -19,7 +23,7 @@ class ClientGUI:
         self.username_entry.pack(pady=5)
         tk.Button(root, text="Conectar", command=self.connect_to_server).pack(pady=5)
 
-        # Response area
+        # Area de resposta
         self.response_text = scrolledtext.ScrolledText(root, width=50, height=10)
         self.response_text.pack(pady=10)
 
@@ -40,6 +44,7 @@ class ClientGUI:
         self.slot_entry = tk.Entry(root)
         self.slot_entry.pack(pady=5)
 
+    # Função para conecer com o servidor 
     def connect_to_server(self):
         username = self.username_entry.get().strip()
         if not username or username.isdigit():
@@ -48,30 +53,36 @@ class ClientGUI:
 
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.connect(("127.0.0.1", 8888))
+            self.client_socket.connect((ip, port))
             self.client_socket.send(username.encode("utf-8"))
             self.username = username
             self.response_text.insert(tk.END, f"Conectado como {username}\n")
         except Exception as e:
             messagebox.showerror("Erro", f"Falha na conexão: {e}")
 
+    # Envio de pedidos ao servidor, para obter a resposta do processo
     def send_request(self, request):
+        # Apresenta um erro caso o Cliente não se conecte
         if not self.client_socket:
             messagebox.showerror("Erro", "Não conectado ao servidor.")
             return None
         try:
+            # Envia e recebe os pedidos
             self.client_socket.send(request.encode("utf-8"))
             response = self.client_socket.recv(4096).decode("utf-8")
             return response
+        
         except Exception as e:
             messagebox.showerror("Erro", f"Erro na comunicação: {e}")
             return None
 
+    # Listar slots
     def list_slots(self):
         response = self.send_request("LIST")
         if response:
             self.response_text.insert(tk.END, response + "\n")
 
+    # Reservar slots
     def book_slot(self):
         slot = self.slot_entry.get().strip()
         if not slot.isdigit():
@@ -81,11 +92,13 @@ class ClientGUI:
         if response:
             self.response_text.insert(tk.END, response + "\n")
 
+    # Ver reservas
     def view_bookings(self):
         response = self.send_request("VIEW")
         if response:
             self.response_text.insert(tk.END, response + "\n")
 
+    # Cancelar Reservas
     def cancel_booking(self):
         slot = self.slot_entry.get().strip()
         if not slot.isdigit():
@@ -95,11 +108,13 @@ class ClientGUI:
         if response:
             self.response_text.insert(tk.END, response + "\n")
 
+    # Listar logs
     def view_logs(self):
         response = self.send_request("LOGS")
         if response:
             self.response_text.insert(tk.END, response + "\n")
 
+    # Apagar todos os dados do utilizador
     def delete_data(self):
         if messagebox.askyesno("Confirmação", "Tem certeza que deseja apagar todos os dados?"):
             response = self.send_request("DELETE")
--- Servidor.py
+++ Servidor.py
@@ -5,7 +5,7 @@ from datetime import date, time, datetime
 
 # Configurações do servidor
 ip_servidor = "127.0.0.1"
-porta = 8888
+porta = 8080
 
 # Dados compartilhados
 available_slots = [time(hour) for hour in range(8, 21)]  # Slots de 8:00 a 20:00
@@ -15,6 +15,7 @@ lock = threading.Lock()
 current_date = date.today()
 current_datetime = datetime.now()
 
+# Recebe os pedidos vindo dos Clientes
 def handle_client(client_socket, address, username):
     print(f"Conexão aceita de {address[0]}:{address[1]} - User: {username}")
     try:
@@ -35,75 +36,105 @@ def handle_client(client_socket, address, username):
         client_socket.close()
         print(f"Conexão com {username} encerrada.")
 
+# Processa os pedidos vindo dos Clientes
 def process_request(request, username):
     global available_slots, bookings
     parts = request.split("|")
     command = parts[0].upper()
 
     with lock:
+        # Listar os slots
         if command == "LIST":
             slots_str = "\n".join([f">> {slot.strftime('%H:%M:%S')}" for slot in available_slots])
             
+            # Registra a listagem no ficheiro texto "audit_nome do utilizador"
             with open(f"audit_{username}.txt", "a") as f:
                     f.write(f"{current_datetime} | LIST_SLOTS | SYSTEM | {current_date}\n")
             
             return f"\nSlots disponíveis:\n{slots_str}"
         
 
+        # Reservar os slot/hora
         elif command == "BOOK":
             if len(parts) < 2:
                 return "Erro: Hora não fornecida."
             try:
                 hour = int(parts[1])
                 slot_time = time(hour)
+
+                # Verifica se o slot/hora está disponivel na lista de slots disponiveis
                 if slot_time not in available_slots:
+
                     with open(f"audit_{username}.txt", "a") as f:
                         f.write(f"{current_datetime} | BOOK_FAILED | {username} | slot::{current_date}:{slot_time} - already booked\n")
+
                     return "Slot indisponível ou já reservado."
+                
+                # Remove o slot/hora quando for reservado
                 available_slots.remove(slot_time)
+
+                # Evita reservas duplicadas 
                 if username not in bookings:
                     bookings[username] = []
                 bookings[username].append(slot_time)
-                # Log
+
+                # Log da reserva
                 with open(f"audit_{username}.txt", "a") as f:
                     f.write(f"{current_datetime} | BOOK_SUCCESS | {username} | slot::{current_date}:{slot_time}\n")
                 return f"Reserva realizada: {current_date} às {slot_time.strftime('%H:%M:%S')}"
+            
             except ValueError:
                 return "Hora inválida."
 
+        # Lista as reservas realizadas pelo user
         elif command == "VIEW":
             user_bookings = bookings.get(username, [])
+
+            # Verifica se as reservas está vazia
             if not user_bookings:
                 with open(f"audit_{username}.txt", "a") as f:
                     f.write(f"{current_datetime} | LIST_USER_BOOKINGS | {username} | count:{len(bookings[username])}\n")
+            
                 return "Nenhuma reserva."
+            
             bookings_str = "\n".join([f"slot::{current_date}:{slot.strftime('%H:%M:%S')}" for slot in user_bookings])
+            
             with open(f"audit_{username}.txt", "a") as f:
                     f.write(f"{current_datetime} | LIST_USER_BOOKINGS | {username} | count:{len(bookings[username])}\n")
+            
             return f"\nSuas reservas:\n{bookings_str}"
 
+        # Processo responsavel por cancelar as reservas
         elif command == "CANCEL":
+            # Verifica se o input está vazio
             if len(parts) < 2:
                 return "Erro: Hora não fornecida."
             try:
                 hour = int(parts[1])
                 slot_time = time(hour)
+                
+                # Verifica se existe uma reserva há ser cancelada
                 if username not in bookings or slot_time not in bookings[username]:
                     with open(f"audit_{username}.txt", "a") as f:
                         f.write(f"{current_datetime} | CANCEL_FAILED | {username} | slot::{current_date}:{slot_time} - not found\n")
                     return "Reserva não encontrada."
+                
+                # remove o slot e o retorna como disponivel
                 bookings[username].remove(slot_time)
                 available_slots.append(slot_time)
                 available_slots.sort()
-                # Log
+                
+                # Log de cancelamento
                 with open(f"audit_{username}.txt", "a") as f:
                     f.write(f"{current_datetime} | CANCEL_SUCCESS | {username} | slot::{current_date}:{slot_time}\n")
                 return f"Reserva cancelada: {current_date} às {slot_time.strftime('%H:%M:%S')}"
+            
             except ValueError:
                 return "Hora inválida."
 
         elif command == "LOGS":
             try:
+                # Listar os logs 
                 with open(f"audit_{username}.txt", "r") as f:
                     logs = f.read()
                 return logs if logs else "Nenhum log."
@@ -119,7 +150,7 @@ def process_request(request, username):
             except FileNotFoundError:
                 pass
             return "Dados apagados."
-
+        
         else:
             return "Comando inválido."
 
@@ -135,6 +166,7 @@ def main():
             client_socket, address = servidor.accept()
             username = client_socket.recv(1024).decode("utf-8")
             threading.Thread(target=handle_client, args=(client_socket, address, username)).start()
+
     except KeyboardInterrupt:
         print("Servidor encerrado.")
 
